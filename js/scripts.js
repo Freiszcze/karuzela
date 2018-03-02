bbed60cd9d6d0fe93c187337cf5897d94bf01f68
Stanisław Gacki <selevar1992@gmail.com>
2018-03-02 04:39:17 +0100

    carousel

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function(){
+    var carouselList = $('#carousel ul');
+    function moveFirstSlide() {
+        var firstItem = carouselList.find("li:first"),
+            lastItem = carouselList.find("li:last");
+        
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft: 0})
+    }
+    function changeSlide() {
+        carouselList.animate({ 'marginLeft': -400 }, 3000, moveFirstSlide);
+    }
+    window.setInterval(changeSlide, 3000);
+});
